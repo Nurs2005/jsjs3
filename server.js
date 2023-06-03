0c926ccf2276f1e24053a5b597643e6444cadc6f
Nurs2005 <nursultan250605@gmail.com>
2023-06-04 02:56:03 +0300

    jsss3
    qwe

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs/promises');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/:str', async (req, res) => {
+  const str = req.params.str;
+
+  try {
+    const data = await fs.readFile(`${str}.txt`, 'utf8');
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.send('Извините, ваш запрос недоступен');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
